04bf4443b0ac72445f093479b24df16af68904a6
Prasanth BJ <prasanthbj65@gmail.com>
2025-10-30 11:20:56 +0530

    Created a Layout for MacbookPro

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from "./Components/Hero"
 import Navbar from "./Components/Navbar"
+import ProductViewer from "./Components/ProductViewer"
 
 const App = () => {
   return (
     <main>
       <Navbar/>
       <Hero/>
+      <ProductViewer/>
     </main>
   )
 }
